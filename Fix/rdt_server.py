231e36772b6017e0b2446e700fcf44579d84439f
Matheus Batista <matheus.vbatista04@gmail.com>
2023-09-29 15:55:51 -0300

    refactor

--- Fix/rdt_server.py
+++ Fix/rdt_server.py
@@ -21,6 +21,7 @@ class RDT_Server:
         self.BUFFER_SIZE = 1024
         self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server.bind(('localhost', 9999))
+        self.friend_list = []
 
     # envia ACK correto
     def send_ACK(self, num_seq, client_address):
@@ -45,6 +46,50 @@ class RDT_Server:
                     self.messages.put((message, address))
             except:
                 pass
+
+    def handle_message(self, message_content, local_time, name_request, client, i, in_ban_index, message, in_ban_name):
+        # se a mensagem for um bye
+        if message_content == " bye":
+            self.send_pkt(f'[{local_time}] {name_request} saiu do chat! :('.encode(), client, i)
+
+        # se o a mensagem for um comando de listagem
+        elif message_content == " list":
+            if name_request == self.clients_names[i]:
+                self.send_pkt(f'[{local_time}] {self.clients_names}'.encode(), client, i)
+
+        elif message_content == " mylist":
+            if name_request == self.clients_names[i]:
+                self.send_pkt(f'[{local_time}] {self.friend_list[i]}'.encode(), client, i)
+        
+        elif message_content.startswith(" addtomylist"):
+            if name_request == self.clients_names[i]:
+                add_name = (message.decode().split(':')[1].strip()).split('@')[1]
+                if add_name in self.clients_names:
+                    self.friend_list[i].append(add_name)
+        
+        elif message_content.startswith(" rmvfrommylis"):
+            if name_request == self.clients_names[i]:
+                add_name = (message.decode().split(':')[1].strip()).split('@')[1]
+                if add_name in self.clients_names:
+                    self.friend_list[i].append(add_name)
+
+        # se a mensagem for uma mensagem privada, envia apenas para o destinatário
+        elif (message_content.strip()).startswith(f'@{self.clients_names[i]}'):
+            self.send_pkt(f'[{local_time}] {name_request}:{message_content}'.encode(), client, i)
+
+        # envia mensagem se um voto for adicionado
+        elif self.vote_checker:
+            self.send_pkt(f'[{local_time}] {message.decode()}'.encode(), client, i)
+            self.send_pkt(f'[{local_time}] {len(self.ban_counter[in_ban_index])}/{len(self.clients_names)} - ban {in_ban_name}'.encode(), client, i)
+        
+        # se a mensagem nao for privada, envia para todos
+        else:
+            self.send_pkt(f'[{local_time}] {message.decode()}'.encode(), client, i)
+        
+        # envia mensagem se um jogador for banido
+        if self.ban_checker:
+            self.send_pkt(f'[{local_time}] O usuario {self.clients_names[in_ban_index]} foi banido!!!'.encode(), client, i)
+
 
     def broadcast(self):
         banned_index = -1
@@ -98,80 +143,91 @@ class RDT_Server:
                                 self.vote_checker = True
 
                 # envio de mensagens para todos os clientes
-                for i, client in enumerate(self.clients):
-                    # primeira mensagem de um novo cliente
-                    if message.decode().startswith('hi, meu nome eh:'):
-                        name = message.decode().split(':')[1]
-                        
-                        # se o cliente estiver na lista de banimentos e tenta enviar uma msg, os outros recebem um aviso
-                        if (name in self.banned_names):
-                            self.send_pkt(f'[{local_time}] O usuario {name} esta banido, nao pode entrar'.encode(), client, i)
-                            if self.clients[i] == address:       # se o usuario que tentou entrar estive banido
-                                self.clients.pop(i)
-                                self.num_seq_list.pop(i)
-                                self.expected_num_seq.pop(i)
+                self.send_to_all_clients(message=message, local_time=local_time, address=address, in_ban_index=in_ban_index, in_ban_name=in_ban_name)
 
-                        # se nao tiver, adiciona o cliente nas listas e envia a mensagem aos usuarios
-                        else:
-                            if i == 0:
-                                self.clients_names.append(name)
-                                self.ban_counter.append([])
-                            self.send_pkt(f'[{local_time}]hi, meu nome eh:{name}'.encode(), client, i)
-                    else:
-                        message_content = message.decode().split(':')[1]
-                        name_request = message.decode().split(':')[0]
-                        # se o nome de quem enviou a mensagem estiver na lista de banimento, ele recebe um aviso
-                        if name_request in self.banned_names:
-                            self.send_pkt(f'[{local_time}] Você esta banido, nao pode enviar mensagens'.encode(), address, 1)
-                            break
-                        
-                        else:
-                            # se a mensagem for um bye
-                            if message_content == " bye":
-                                self.send_pkt(f'[{local_time}] {name_request} saiu do chat! :('.encode(), client, i)
-
-                            # se o a mensagem for um comando de listagem
-                            elif message_content == " list":
-                                if name_request == self.clients_names[i]:
-                                    self.send_pkt(f'[{local_time}] {self.clients_names}'.encode(), client, i)
-                            
-                            # se a mensagem for uma mensagem privada, envia apenas para o destinatário
-                            elif (message_content.strip()).startswith(f'@{self.clients_names[i]}'):
-                                self.send_pkt(f'[{local_time}] {name_request}:{message_content}'.encode(), client, i)
-
-                            # envia mensagem se um voto for adicionado
-                            if self.vote_checker:
-                                self.send_pkt(f'[{local_time}] {len(self.ban_counter[in_ban_index])}/{len(self.clients_names)} - ban {in_ban_name}'.encode(), client, i)
-                            
-                            # envia mensagem se um jogador for banido
-                            if self.ban_checker:
-                                self.send_pkt(f'[{local_time}] O usuario {self.clients_names[in_ban_index]} foi banido!!!'.encode(), client, i)
-                            
-                            # se a mensagem nao for privada, envia para todos
-                            if not (message_content.strip()).startswith(f'@'):
-                                self.send_pkt(f'[{local_time}] {message.decode()}'.encode(), client, i)
-
+                # Retira o usuário da lista caso ele saia do chat
+                self.handle_bye_message(message=message)
+                
                 # retira o usuário da lista caso ele seja banido ou saia do chat
-                if len(self.ban_counter[banned_index]) >= 2*len(self.clients_names)/3:
-                    self.banned_names.append(self.clients_names[banned_index])
-                    self.clients.pop(banned_index)
-                    self.clients_names.pop(banned_index)
-                    self.ban_counter.pop(banned_index)
-                    self.num_seq_list.pop(banned_index)
-                    self.expected_num_seq.pop(banned_index)
-                    self.ban_timer.pop(banned_index)
-                    banned_index = 0
+                self.check_ban(banned_index=banned_index)
+                
 
-                # retira o usuário da lista caso ele saia do chat
-                if message.decode().split(':')[1] == " bye":
-                    index = self.clients_names.index(name_request)
-                    self.clients.pop(index)
-                    self.clients_names.pop(index)
-                    self.ban_counter.pop(index)
-                    self.num_seq_list.pop(index)
-                    self.expected_num_seq.pop(index)
-                    self.ban_timer.pop(index)
+    def handle_first_message_banned_user(self, message, local_time, client, address, i):
+        name = message.decode().split(':')[1]
+        if (name in self.banned_names):
+            self.send_pkt(f'[{local_time}] O usuario {name} esta banido, nao pode entrar'.encode(), client, i)
+            if self.clients[i] == address:       # se o usuario que tentou entrar estive banido
+                self.clients.pop(i)
+                self.num_seq_list.pop(i)
+                self.expected_num_seq.pop(i)
+                self.friend_list.pop(i)
 
+    def handle_first_message_action(self,message, i, local_time, client):
+        name = message.decode().split(':')[1]             
+        if i == 0:
+            self.clients_names.append(name)
+            self.friend_list.append([])
+            self.ban_counter.append([])
+        self.send_pkt(f'[{local_time}]hi, meu nome eh:{name}'.encode(), client, i)
+
+    def handle_first_message(self, message, local_time, client, i, address):
+        name = message.decode().split(':')[1]         
+        # se o cliente estiver na lista de banimentos e tenta enviar uma msg, os outros recebem um aviso
+        if (name in self.banned_names):
+            self.handle_first_message_banned_user(message=message, local_time=local_time, client=client, address=address, i=i)
+
+        # se nao tiver, adiciona o cliente nas listas e envia a mensagem aos usuarios
+        else:
+            self.handle_first_message_action(message, i, local_time, client)
+    
+    def handle_common_message(self, message, local_time, client, i, in_ban_index, in_ban_name, address):
+        message_content = message.decode().split(':')[1]
+        name_request = message.decode().split(':')[0]
+        # se o nome de quem enviou a mensagem estiver na lista de banimento, ele recebe um aviso
+        if name_request in self.banned_names:
+            self.send_pkt(f'[{local_time}] Você esta banido, nao pode enviar mensagens'.encode(), address, 1)
+            return True
+        else:
+            self.handle_message(message_content, local_time, name_request, client, i, in_ban_index, message, in_ban_name)
+            return False
+        
+    
+    def send_to_all_clients(self, message, local_time, address, in_ban_index, in_ban_name):
+        for i, client in enumerate(self.clients):
+            print(message.decode().split(':')[0])
+            print(message.decode().split(':')[1].strip())
+            # primeira mensagem de um novo cliente
+            if message.decode().startswith('hi, meu nome eh:'):
+                self.handle_first_message(message, local_time, client, i, address)
+            else:
+                isBanned = self.handle_common_message(message=message, local_time=local_time, client=client, i=i, in_ban_index=in_ban_index, in_ban_name=in_ban_name, address=address)
+                if(isBanned): break
+
+    
+    def handle_bye_message(self, message):
+        if message.decode().split(':')[1] == " bye":
+            name_request = message.decode().split(':')[0]
+            print(name_request)
+            index = self.clients_names.index(name_request)
+            self.clients.pop(index)
+            self.clients_names.pop(index)
+            self.ban_counter.pop(index)
+            self.num_seq_list.pop(index)
+            self.expected_num_seq.pop(index)
+            self.ban_timer.pop(index)
+        
+    def check_ban(self, banned_index):
+        if len(self.ban_counter[banned_index]) >= 2*len(self.clients_names)/3:
+            self.banned_names.append(self.clients_names[banned_index])
+            self.clients.pop(banned_index)
+            self.clients_names.pop(banned_index)
+            self.ban_counter.pop(banned_index)
+            self.num_seq_list.pop(banned_index)
+            self.expected_num_seq.pop(banned_index)
+            self.ban_timer.pop(banned_index)
+            banned_index = 0
+    
+    
     def start(self):
         t1 = threading.Thread(target=self.receive_message)
         t2 = threading.Thread(target=self.broadcast)
